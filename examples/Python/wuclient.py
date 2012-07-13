00c8eb0aefaaea43decb593dc8cd042fd70fb998
Eddie Hebert <eddie@eddiehebert.com>
2012-07-13 14:01:09 -0400

    Applies PEP-8 fixes.
    - Removes space between method signature and '('
    - Removes trailing white space.

--- examples/Python/wuclient.py
+++ examples/Python/wuclient.py
@@ -12,7 +12,7 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
 print "Collecting updates from weather server..."
-socket.connect ("tcp://localhost:5556")
+socket.connect("tcp://localhost:5556")
 
 # Subscribe to zipcode, default is NYC, 10001
 zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
@@ -20,10 +20,10 @@ socket.setsockopt(zmq.SUBSCRIBE, zip_filter)
 
 # Process 5 updates
 total_temp = 0
-for update_nbr in range (5):
+for update_nbr in range(5):
     string = socket.recv()
     zipcode, temperature, relhumidity = string.split()
     total_temp += int(temperature)
 
-print "Average temperature for zipcode '%s' was %dF" % (           
+print "Average temperature for zipcode '%s' was %dF" % (
       zip_filter, total_temp / update_nbr)
